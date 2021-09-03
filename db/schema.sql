619b444b3618aade03dbd33da84e7d9b79d41c34
kamm8899 <kamm8899@gmail.com>
2021-09-03 15:46:04 -0700

    added seed.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,24 @@ CREATE DATABASE employee_db;
 
 USE employee_db
 
-CREATE table viewDepartment(
+CREATE table department(
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     department_name VARCHAR(25) NOT NULL,
 );
-CREATE table viewRoles(
+CREATE table roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     job_titles VARCHAR(25) NOT NULL,
     department_name VARCHAR(25) NOT NULL,
     salary INTEGER
+    department_id INTEGER
 );
-CREATE table viewEmployees(
+CREATE table employees(
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
-    job_titles VARCHAR(25) NOT NULL,
+    role_id VARCHAR(25) NOT NULL,
     salaries INTEGER NOT NULL,
-    managers VARCHAR(25) NOT NULL,
+    manager_id VARCHAR(25) NOT NULL,
 );
 
 
